547dd11f4de00da5fd60dde12c727dc6c1ca0318
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-04-15 21:54:37 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+let form = document.querySelector(".form");
+form.addEventListener('submit',(e) => {
+    const fields = {
+        card: document.getElementById("cardNumber"),
+        cvc: document.getElementById("cvcNumber"),
+        amount: document.getElementById("amount"),
+        firstName: document.getElementById("first_name"),
+        lastName: document.getElementById("last_name"),
+        city: document.getElementById("city"),
+        state: document.getElementById("state"),
+        postalCode: document.getElementById("postal_code"),
+        cardType: document.getElementById("cardType")
+    }
+    let Error = false;
+    const cardTypeChecked = document.querySelector('input[name="payment_method"]:checked');
+    const alert = document.querySelector('.alert');
 
+    Object.values(fields).forEach(field => {
+        if (field.value === "" || field.selectedIndex === 0 || (field.id === 'cardType' && !cardTypeChecked)) {
+            field.classList.add('red');
+            Error = true;
+        } else {
+            field.classList.remove('red');
+        }
+    });
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    if (Error) {
+        e.preventDefault();
+        alert.classList.remove("d-none");
+        console.log("Form has validation errors");
+    } else {
+        alert.classList.add("d-none");
+        console.log("Form submitted");
+    }
+})
